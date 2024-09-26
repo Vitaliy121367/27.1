cd89b24d42974e6a4516f0ca0f8cba2da7a220b9
Vitaliy <qw929496@gmail.com>
2024-09-26 18:10:55 +0300

    MusicCD cpp

--- MusicCD.cpp
+++ MusicCD.cpp
@@ -2,21 +2,29 @@
 
 MusicCD::MusicCD()
 {
+    time = 0;
 }
 
 MusicCD::MusicCD(string title, string author, int year, float price, int time)
+    :Item(title, author, year, price)
 {
+    this->time = time;
 }
 
 void MusicCD::setTime(int time)
 {
+    this->time = time;
 }
 
 int MusicCD::getTime() const
 {
-    return 0;
+    return time;
 }
 
 void MusicCD::showInfo() const
 {
+    cout << "\n---------------------------------------\n";
+    cout << "\tMusicCD\n";
+    Item::showInfo();
+    cout << "Time: " << time << endl << endl;
 }
