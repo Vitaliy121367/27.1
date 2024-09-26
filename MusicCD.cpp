d7cf722719945057e5083ed32913184e564b2bef
Vitaliy <qw929496@gmail.com>
2024-09-26 18:06:10 +0300

    class MusicCD h

---
+++ MusicCD.cpp
@@ -0,0 +1,22 @@
+#include "MusicCD.h"
+
+MusicCD::MusicCD()
+{
+}
+
+MusicCD::MusicCD(string title, string author, int year, float price, int time)
+{
+}
+
+void MusicCD::setTime(int time)
+{
+}
+
+int MusicCD::getTime() const
+{
+    return 0;
+}
+
+void MusicCD::showInfo() const
+{
+}
